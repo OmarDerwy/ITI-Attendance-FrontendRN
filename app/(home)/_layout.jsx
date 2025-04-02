b0a4f533b79197defe7cd886535b0f3627b7f458
OmarDerwy <OmarDerwy@gmail.com>
2025-04-02 16:20:21 +0200

    chore(files): change naming of things before merge

--- app/(home)/_layout.jsx
+++ app/(home)/_layout.jsx
@@ -1,7 +1,5 @@
 import { Stack } from 'expo-router/stack'
 
-// TODO checkout name of (home) to home
-
 export default function Layout() {
   return( 
   <Stack screenOptions={{animation: 'slide_from_right'}}>
