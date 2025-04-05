9a7aac3ae0c9c0649bced2e44b4e5e6a5086ef59
Menna Reda <menna.r712@gmail.com>
2025-04-05 16:26:47 +0200

    push notifications without build

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,25 @@
 import { Stack } from "expo-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { NotificationProvider } from "./context/NotificationsContext";
+import * as Notifications from "expo-notifications";
 // DONE implement some form of caching and state management
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
+
 const queryClient = new QueryClient();
 export default function RootLayout() {
   return (
+    <NotificationProvider>
       <QueryClientProvider client={queryClient}>
         <Stack screenOptions={{ headerShown: false }}>
           <Stack.Screen name="index"/>
         </Stack>
       </QueryClientProvider>
+    </NotificationProvider>
   );
 }
